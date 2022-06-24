afc811338dc1bad12a0100292186d2ead0790106
devendra vyas <devendravyas04@gmail.com>
2022-06-24 17:36:02 +0100

    Project Skills Unfold initiated

---
+++ config/webpack/environment.js
@@ -0,0 +1,10 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.append('provide',
+  new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+  })
+)
+module.exports = environment
